3fd4d7d67cba1772679416635c89784b8027620b
samuelsehnert <samuel.sehenrt@gmail.com>
2022-10-29 14:58:47 -0700

    looks good now

--- react-frontend/src/AccountCreation.jsx
+++ react-frontend/src/AccountCreation.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import "./AccountCreation.css"
 
@@ -15,16 +15,19 @@ function AccountCreation() {
   const checkFieldsValid = () => email.length !== 0 && password.length !== 0 && confirmPasword.length !== 0
   const checkEmailValid = () => /.+@.+\.[A-Za-z]+$/.test(email); // Complicated regex. Don't worry about it...
 
-  const goButtonSubmitted = () => {
+  useEffect(() => {
     if (!checkFieldsValid()) return;
 
     // Pulled out err to a seperate variables because setState is async. Causes race condition
     const err = { emailInvalid: false, passwordsDontMatch: false };
-    if (!checkEmailValid()) { err.emailInvalid = true;}
-    if (password.localeCompare(confirmPasword)) { err.passwordsDontMatch = true; }
+    if (email !== "" && !checkEmailValid()) { err.emailInvalid = true;}
+    if (password !== "" && confirmPasword !== "" && password.localeCompare(confirmPasword)) { err.passwordsDontMatch = true; }
 
     setErrors(err);
-    if (err.emailInvalid || err.passwordsDontMatch) { return; }
+  }, [email, password, confirmPasword])
+
+  const goButtonSubmitted = () => {
+    if (errors.emailInvalid || errors.passwordsDontMatch) { return; }
 
     console.debug("Congrats!");
     console.debug("email:", email);
@@ -34,15 +37,16 @@ function AccountCreation() {
 
   return (
     <div className="account-creation-page-parent">
+      <div className="title"><span className="test">Box</span>Office</div>
       <div className="account-creation-email-input">
-        <input className="input-box" type="email" name="email" placeholder="Email" onChange={e => setEmail(e.target.value)}/>
+        <input className={!errors.emailInvalid ? "input-box" : "input-box-error"} type="email" name="email" placeholder="Email" onChange={e => setEmail(e.target.value)}/>
         { errors.emailInvalid && 
           <div className="invalid">Email is Invalid</div>
         }
       </div>
       <div className="account-creation-password-input">
-        <input className="input-box" type="password" name="password" placeholder="Pasword" onChange={e => setPassword(e.target.value)}/>
-        <input className="input-box" type="password" name="confirmPassword" placeholder="Confirm Password" onChange={e => setConfirmPasword(e.target.value)}/>
+        <input className={!errors.passwordsDontMatch ? "input-box" : "input-box-error"} type="password" name="password" placeholder="Pasword" onChange={e => setPassword(e.target.value)}/>
+        <input className={!errors.passwordsDontMatch ? "input-box" : "input-box-error"} type="password" name="confirmPassword" placeholder="Confirm Password" onChange={e => setConfirmPasword(e.target.value)}/>
         { errors.passwordsDontMatch &&
           <div className="invalid">Passwords don&apos;t match</div>
       }
