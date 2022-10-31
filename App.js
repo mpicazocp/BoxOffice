2704e67ccda34779450d4fb3976cbd1371e13a81
mpicazocp <mpicazo@calpoly.edu>
2022-10-31 08:30:57 -0700

    added searchBar

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+import SearchBar from './searchBar'
+
+
+function App() {
+    return (
+       <div classname='App'>
+         <SearchBar/>
+       </div>
+  )}export default App
