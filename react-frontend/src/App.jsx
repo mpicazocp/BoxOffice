80e2f21351e813b46a8489fd7337a54cad0a1b71
samuelsehnert <samuel.sehenrt@gmail.com>
2022-11-01 22:18:08 -0700

    init

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import AccountCreation from "./AccountCreation";
+import AccountCreation from './AccountCreation';
+import LoginPage from './LoginPage';
 
 function App() {
 
   return (
-    <div className="App">
-      <AccountCreation />
+    <div className='App'>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/newAccount' element={<AccountCreation />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
