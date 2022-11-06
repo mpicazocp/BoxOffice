edd65719087ddd4bd25c5d1ad9efb1f35745f64f
mpicazocp <mpicazo@calpoly.edu>
2022-11-06 07:41:48 -0800

    EDIT BUTTON

--- react-frontend/src/showListings.jsx
+++ react-frontend/src/showListings.jsx
@@ -8,8 +8,14 @@ function ListingsPage(){
     // mediaList = [["Movie1", "desc1"], ["movie2", "desc2"]]
 
     const { Paragraph } = Placeholder;
+
+    const HomeButtonSubmit = () => {
+        console.debug("Congrats!");
+    };
+
     return (
         <div className="listings-page-parent">
+            <button className="homeButton" type="submit" onClick={HomeButtonSubmit}>HOME</button>
             <h1 className= "header">Results</h1>
             <div className= "listings" style={{ marginTop: 20, width: 800 }}>
                 <Paragraph style={{ marginTop: 30 }} 
