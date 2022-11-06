495324778fb4066218dfcc8a3fc61ecad3c3284e
samuelsehnert <samuel.sehenrt@gmail.com>
2022-11-05 17:25:01 -0700

    Init and done

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import HomePage from "./HomePage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import ShowInformation from './ShowInformation'
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/sunny' element={
+            <ShowInformation 
+              name="Always Sunny"
+              img="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/IASIPTC.svg/1200px-IASIPTC.svg.png"
+              rating="R"
+              streamingService="Hulu"
+              currentEpisode={4}
+              currentSeason={3}
+              currentTime={23}
+              totalTime={45}
+              desc="Very wacky adventures"
+            />
+          } />
+          <Route path='/shrek' element={
+            <ShowInformation 
+              name="Shrek"
+              img="https://upload.wikimedia.org/wikipedia/en/7/7b/Shrek_%282001_animated_feature_film%29.jpg"
+              rating="PG-13"
+              streamingService="Peacock"
+              isMovie
+              currentTime={10}
+              totalTime={100}
+              desc="Very swampy adventures"
+            />
+          } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
